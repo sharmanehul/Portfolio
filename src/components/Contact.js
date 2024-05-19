9db29e6cc770c6e0262a71eee9046c4f68da4aa4
NEHUL SHARMA <sharmanehul699@gmail.com>
2024-05-19 23:51:25 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -47,7 +47,7 @@ export const Contact = () => {
     <section className="contact" id="connect">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} md={7}>
+          <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
                 <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
